539e1236c830fb656022767de4d211cd61c04747
Thomas Huet <t.huet@iia-laval.fr>
2018-10-09 14:40:23 +0200

    conflit resolved confilt & master

--- test/test_app.bat
+++ test/test_app.bat
@@ -1,11 +1,7 @@
 ### application.bat - 2018 - votre nom ###
-<<<<<<< HEAD
+
 version = "1.2.1"
 build_hour = "11H00"
-=======
-version = "1.2.0-b"
-build_hour = "14H00"
->>>>>>> conflit
 
 echo "Build ${build_hour}"
 echo "version ${version}"
